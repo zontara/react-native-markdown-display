fc56f2e5c2acdb31e9206db66dc38e5e875499a1
Daniel Maslowski <info@orangecms.org>
2023-03-09 15:59:16 +0100

    extend ESLint config, add import plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,28 @@
 module.exports = {
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   settings: {
-      react: {
-          version: require('./package.json').peerDependencies.react,
-      },
+    react: {
+      version: require('./package.json').peerDependencies.react,
+    },
+    'import/ignore': ['node_modules'],
   }
 };
